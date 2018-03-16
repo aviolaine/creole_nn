a82b889f5a882c05bb10af160171d9e6dd465cbc
aviolaine <aviotac@gmail.com>
2018-03-16 12:44:35 -0500

    data update

---
+++ creole/neural_stacking/data/en/divide.py
@@ -0,0 +1,71 @@
+import numpy as np
+import os
+
+assert os.path.exists('wiki.txt')
+
+out = ""
+with open('wiki.txt', 'r') as f:
+    #data = f.read().split('\n')
+    data = f.read().splitlines()#get rid of newlines
+    for l in data:
+        out += l
+
+
+from nltk.tokenize import sent_tokenize
+data = sent_tokenize(out)
+#np.random.shuffle(data)
+
+#n = len(data)
+#n = 5000
+n = 110319
+artificial = True
+print("length of data:", n)
+train_end = int(.6*n)
+valid_end = int(.2*n) + train_end
+train_data = data[:train_end]
+valid_data = data[train_end:valid_end]
+test_data = data[valid_end:]
+if artificial:
+    test_data = data[valid_end:n]
+else:
+    test_data = data[valid_end:]
+print("length of train:",len(train_data))
+print("length of valid:",len(valid_data))
+print("length of test:",len(test_data))
+
+import re
+
+with open('train.txt','w') as f:
+    for s in train_data:
+        s = re.sub('([.,!?()])', r' \1 ', s)
+        s = re.sub('\s{2,}', ' ', s)
+        #s = list(s)
+        #if s[len(s)-1] == '\n':
+            #f.write(s)
+        #else:
+        f.write(s+'\n')
+        #if s[len(s)-1] == '.':
+            #s[len(s)-1] = ' '
+            #s+='.'
+        
+
+with open('valid.txt','w') as f:
+    for s in valid_data:
+        s = re.sub('([.,!?()])', r' \1 ', s)
+        s = re.sub('\s{2,}', ' ', s)
+        #f.write(s+'\n')
+        #if s[len(s)-1] == '\n':
+            #f.write(s)
+        #else:
+        f.write(s+'\n')
+
+with open('test.txt','w') as f:
+    for s in test_data:
+        s = re.sub('([.,!?()])', r' \1 ', s)
+        s = re.sub('\s{2,}', ' ', s)
+        #f.write(s+'\n')
+        #if s[len(s)-1] == '\n':
+        #f.write(s)
+        #else:
+        f.write(s+'\n')
+        
--- creole/neural_stacking/data/srn/divide.py
+++ creole/neural_stacking/data/srn/divide.py
@@ -3,17 +3,21 @@ import os
 
 assert os.path.exists('wiki.txt')
 
+out = ""
 with open('wiki.txt', 'r') as f:
     #data = f.read().split('\n')
-    data = f.read()
+    data = f.read().splitlines()#get rid of newlines
+    for l in data:
+        out += l
+
 
 from nltk.tokenize import sent_tokenize
-data = sent_tokenize(data)
+data = sent_tokenize(out)
 #np.random.shuffle(data)
 
 n = len(data)
-#n = 100
-artificial = True
+#n = 5000
+artificial = False
 print("length of data:", n)
 train_end = int(.6*n)
 valid_end = int(.2*n) + train_end
@@ -35,20 +39,32 @@ with open('train.txt','w') as f:
         s = re.sub('([.,!?()])', r' \1 ', s)
         s = re.sub('\s{2,}', ' ', s)
         #s = list(s)
+        #if s[len(s)-1] == '\n':
+            #f.write(s)
+        #else:
+        f.write(s+'\n')
         #if s[len(s)-1] == '.':
             #s[len(s)-1] = ' '
             #s+='.'
-        f.write(s+'\n')
+        
 
 with open('valid.txt','w') as f:
     for s in valid_data:
         s = re.sub('([.,!?()])', r' \1 ', s)
         s = re.sub('\s{2,}', ' ', s)
+        #f.write(s+'\n')
+        #if s[len(s)-1] == '\n':
+            #f.write(s)
+        #else:
         f.write(s+'\n')
 
 with open('test.txt','w') as f:
     for s in test_data:
         s = re.sub('([.,!?()])', r' \1 ', s)
         s = re.sub('\s{2,}', ' ', s)
+        #f.write(s+'\n')
+        #if s[len(s)-1] == '\n':
+        #f.write(s)
+        #else:
         f.write(s+'\n')
         
